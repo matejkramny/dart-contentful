dd1676b3270e863f380000bfc3999a8cf132a8f3
Tim Smart <tim.smart@arisechurch.com>
2019-11-26 12:50:26 +1300

    1.1.0 update deps

--- lib/models/asset.g.dart
+++ lib/models/asset.g.dart
@@ -8,63 +8,79 @@ part of 'asset.dart';
 
 Asset _$AssetFromJson(Map<String, dynamic> json) {
   return Asset(
-      sys: json['sys'] == null
-          ? null
-          : SystemFields.fromJson(json['sys'] as Map<String, dynamic>),
-      fields: json['fields'] == null
-          ? null
-          : AssetFields.fromJson(json['fields'] as Map<String, dynamic>));
+    sys: json['sys'] == null
+        ? null
+        : SystemFields.fromJson(json['sys'] as Map<String, dynamic>),
+    fields: json['fields'] == null
+        ? null
+        : AssetFields.fromJson(json['fields'] as Map<String, dynamic>),
+  );
 }
 
-Map<String, dynamic> _$AssetToJson(Asset instance) =>
-    <String, dynamic>{'sys': instance.sys, 'fields': instance.fields};
+Map<String, dynamic> _$AssetToJson(Asset instance) => <String, dynamic>{
+      'sys': instance.sys,
+      'fields': instance.fields,
+    };
 
 AssetFields _$AssetFieldsFromJson(Map<String, dynamic> json) {
   return AssetFields(
-      title: json['title'] as String,
-      file: json['file'] == null
-          ? null
-          : AssetFile.fromJson(json['file'] as Map<String, dynamic>));
+    title: json['title'] as String,
+    file: json['file'] == null
+        ? null
+        : AssetFile.fromJson(json['file'] as Map<String, dynamic>),
+  );
 }
 
 Map<String, dynamic> _$AssetFieldsToJson(AssetFields instance) =>
-    <String, dynamic>{'title': instance.title, 'file': instance.file};
+    <String, dynamic>{
+      'title': instance.title,
+      'file': instance.file,
+    };
 
 AssetFile _$AssetFileFromJson(Map<String, dynamic> json) {
   return AssetFile(
-      fileName: json['fileName'] as String,
-      contentType: json['contentType'] as String,
-      url: json['url'] as String,
-      details: json['details'] == null
-          ? null
-          : AssetFileDetails.fromJson(json['details'] as Map<String, dynamic>));
+    fileName: json['fileName'] as String,
+    contentType: json['contentType'] as String,
+    url: json['url'] as String,
+    details: json['details'] == null
+        ? null
+        : AssetFileDetails.fromJson(json['details'] as Map<String, dynamic>),
+  );
 }
 
 Map<String, dynamic> _$AssetFileToJson(AssetFile instance) => <String, dynamic>{
       'fileName': instance.fileName,
       'contentType': instance.contentType,
       'url': instance.url,
-      'details': instance.details
+      'details': instance.details,
     };
 
 AssetFileDetails _$AssetFileDetailsFromJson(Map<String, dynamic> json) {
   return AssetFileDetails(
-      size: json['size'] as int,
-      image: json['image'] == null
-          ? null
-          : AssetFileDetailsImage.fromJson(
-              json['image'] as Map<String, dynamic>));
+    size: json['size'] as int,
+    image: json['image'] == null
+        ? null
+        : AssetFileDetailsImage.fromJson(json['image'] as Map<String, dynamic>),
+  );
 }
 
 Map<String, dynamic> _$AssetFileDetailsToJson(AssetFileDetails instance) =>
-    <String, dynamic>{'size': instance.size, 'image': instance.image};
+    <String, dynamic>{
+      'size': instance.size,
+      'image': instance.image,
+    };
 
 AssetFileDetailsImage _$AssetFileDetailsImageFromJson(
     Map<String, dynamic> json) {
   return AssetFileDetailsImage(
-      height: json['height'] as int, width: json['width'] as int);
+    height: json['height'] as int,
+    width: json['width'] as int,
+  );
 }
 
 Map<String, dynamic> _$AssetFileDetailsImageToJson(
         AssetFileDetailsImage instance) =>
-    <String, dynamic>{'height': instance.height, 'width': instance.width};
+    <String, dynamic>{
+      'height': instance.height,
+      'width': instance.width,
+    };
--- lib/models/system_fields.g.dart
+++ lib/models/system_fields.g.dart
@@ -8,22 +8,23 @@ part of 'system_fields.dart';
 
 SystemFields _$SystemFieldsFromJson(Map<String, dynamic> json) {
   return SystemFields(
-      id: json['id'] as String,
-      type: json['type'] as String,
-      space: json['space'] == null
-          ? null
-          : Link.fromJson(json['space'] as Map<String, dynamic>),
-      contentType: json['contentType'] == null
-          ? null
-          : Link.fromJson(json['contentType'] as Map<String, dynamic>),
-      revision: json['revision'] as int,
-      createdAt: json['createdAt'] == null
-          ? null
-          : DateTime.parse(json['createdAt'] as String),
-      updatedAt: json['updatedAt'] == null
-          ? null
-          : DateTime.parse(json['updatedAt'] as String),
-      locale: json['locale'] as String);
+    id: json['id'] as String,
+    type: json['type'] as String,
+    space: json['space'] == null
+        ? null
+        : Link.fromJson(json['space'] as Map<String, dynamic>),
+    contentType: json['contentType'] == null
+        ? null
+        : Link.fromJson(json['contentType'] as Map<String, dynamic>),
+    revision: json['revision'] as int,
+    createdAt: json['createdAt'] == null
+        ? null
+        : DateTime.parse(json['createdAt'] as String),
+    updatedAt: json['updatedAt'] == null
+        ? null
+        : DateTime.parse(json['updatedAt'] as String),
+    locale: json['locale'] as String,
+  );
 }
 
 Map<String, dynamic> _$SystemFieldsToJson(SystemFields instance) =>
@@ -35,5 +36,5 @@ Map<String, dynamic> _$SystemFieldsToJson(SystemFields instance) =>
       'revision': instance.revision,
       'createdAt': instance.createdAt?.toIso8601String(),
       'updatedAt': instance.updatedAt?.toIso8601String(),
-      'locale': instance.locale
+      'locale': instance.locale,
     };
